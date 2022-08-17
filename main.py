ab925c43ce838ef49412707c603d6b4d03455dff
faiz625 <76976203+faiz625@users.noreply.github.com>
2022-08-16 21:52:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,50 @@
 import numpy as np
-import pygame
-import sys
-import math
 
 ROW_TOTAL = 6
 COL_TOTAL = 7
 
 def create_board():
-    layout = np.zeros((ROW_TOTAL,COL_TOTAL))
-    return layout
+	layout = np.zeros((ROW_TOTAL,COL_TOTAL))
+	return layout
 
 def drop_piece(layout, row, column, piece):
-  layout[row][column] = piece
+	layout[row][column] = piece
 
-def is_valid(layout,column):
+def is_valid(layout, column):
 	return layout[5][column] == 0
 
 def open_row(layout, column):
 	for r in range(ROW_TOTAL):
-	  if layout[r][column] == 0:
-	    return r
+		if layout[r][column] == 0:
+			return r
 
-def print_layout(layout):
-  print(np.flip(layout,0))
+def print_board(layout):
+	print(np.flip(layout, 0))
 
 layout = create_board()
-print_layout(layout)
+print_board(layout)
 game_over = False
 turn = 0
 
 while not game_over:
-  
-	#Input for P1 
-	if turn == 0:
-		column = int(input("P1, Choose where you want to place your piece (0-6):"))
-		if is_valid(layout,column):
-		  row = open_row(layout, column)
-		  drop_piece(layout, row, column, 1)
+	#P1 input
 
-	#Input for P2
-	
-else: 
-		column = int(input("P2, Choose where you want to place your piece (0-6):"))
+	if turn == 0:
+		column = int(input("P1, please choose where you want to put your piece (0-6):"))
+
 		if is_valid(layout, column):
-		  row = open(layout, column)
-		  drop_piece(layout, row, column, 2)	
-	  
-print_layout(layout)
-	
-turn += 1
-turn = turn%2
+			row = open_row(layout, column)
+			drop_piece(layout, row, column, 1)
+
+	#P2 input
+	else:
+		column = int(input("P2, please choose where you want to put your piece (0-6):"))
+
+		if is_valid(layout, column):
+			row = open_row(layout, column)
+			drop_piece(layout, row, column, 2)
+
+	print_board(layout)
+
+	turn += 1
+	turn = turn % 2
